45f091df04c65c5ab2e875f8bc1834507c6f7b5a
samutamm <samutamm@gmail.com>
2015-04-27 18:24:56 +0300

    OMDB service, controller and form.

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,5 @@
 var ElokuvaApp = angular.module('ElokuvaApp', ['firebase', 'ngRoute']);
+
+ElokuvaApp.config(['$httpProvider', function($httpProvider) {
+  delete $httpProvider.defaults.headers.common["X-Requested-With"]
+}]);
--- web/app/controllers/ListMoviesController.js
+++ web/app/controllers/ListMoviesController.js
@@ -1,10 +1,23 @@
-angular.module('ElokuvaApp').controller('ListMoviesController', function(WindowDialogService, FirebaseService, $scope, $location) {
-    $scope.movies = FirebaseService.getMovies();   
-    
-    $scope.remove = function(index) {
-        if(WindowDialogService.confirm("Are you sure?")) {
+angular.module('ElokuvaApp').controller('ListMoviesController', function (OMDBService, WindowDialogService, FirebaseService, $scope, $location) {
+    $scope.movies = FirebaseService.getMovies();
+
+    $scope.remove = function (index) {
+        if (WindowDialogService.confirm("Are you sure?")) {
             FirebaseService.removeMovie(index);
             $location.path('/movies');
         }
-    } 
+    }
+
+    $scope.searchMovies = function (search) {
+        OMDBService.findMovie(search.name, search.year).success(function (movies) {
+            $scope.OMDBmovies = movies;
+            if ($scope.OMDBmovies.Response === "False") {
+                $scope.moviesFound = false;
+            } else {
+                search = {};
+                $scope.moviesFound = true;
+            }
+            $scope.searchDone = true;
+        });
+    }
 });
